eb11130c4d130367448387752008af1d44656a44
uglybugger <andrewh@uglybugger.org>
2012-06-15 17:26:04 +1000

    Added NuGet package generation on build.

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyDescription("Yet Another Command-Line Parser")]
+[assembly: AssemblyCompany("Andrew Harcourt")]
+[assembly: AssemblyProduct("YACLP")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTrademark("")]
--- src/Yaclp.Build/Properties/AssemblyInfo.cs
+++ src/Yaclp.Build/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Yaclp")]
-[assembly: Guid("c41093a9-5f4e-42f3-9742-cf2a3cf17c61")]
+[assembly: AssemblyTitle("Yaclp.Build")]
+[assembly: Guid("d1747fc9-35e7-4248-a3a8-dace05a967cc")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
