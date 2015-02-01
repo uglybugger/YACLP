8ed9e4e59111452e3ba199478a8a0b71ff3b5ee0
Andrew Harcourt <andrewh@uglybugger.org>
2015-02-01 16:35:15 +1000

    Removed custom build project. TeamCity will do that for us now.

--- src/Yaclp.Build/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Yaclp.Build")]
-[assembly: Guid("d1747fc9-35e7-4248-a3a8-dace05a967cc")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: ComVisible(false)]
